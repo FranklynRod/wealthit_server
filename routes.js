4247589d31d269605042a313b6bd6439965be392
Franklyn Rodriguez <92125381+FranklynRod@users.noreply.github.com>
2024-01-14 12:07:58 -0500

    created more routes

--- routes.js
+++ routes.js
@@ -6,7 +6,7 @@ const Liability = require('./models').Liability;
 const Asset =  require('./models').Asset;
 
 
-//Async Handler
+//Async Handler is a helper function to handle errors
 function asyncHandler(cb){
     return async(req,res,next) =>{
       try{
@@ -35,7 +35,7 @@ router.get('/account/:id', asyncHandler(async(req, res, next) => {
   }));
 
 //GET AssetID
-router.get('/asset/:id', asyncHandler(async(req, res, next) => {
+router.get('account/:account-id/asset/:asset-id', asyncHandler(async(req, res, next) => {
     const asset = await Asset.findByPk(req.params.id ,{
       include: {
         model: Asset,
@@ -52,7 +52,7 @@ router.get('/asset/:id', asyncHandler(async(req, res, next) => {
   }));
 
 //GET LiabilityID
-router.get('/liability/:id',asyncHandler(async(req, res, next)=>{
+router.get('account/:account-id/liability/:liability-id',asyncHandler(async(req, res, next)=>{
     const liability = await Liability.findByPk(req.params.id,{
         include:{
             model: Liability,
@@ -62,20 +62,152 @@ router.get('/liability/:id',asyncHandler(async(req, res, next)=>{
     if (liability){
         res.json(liability).status(200);
     } else{
-        const error = new Error("The liability your trying to fine does not exist")
+        const error = new Error("The liability your trying to find does not exist")
         error.status = 404;
         next(error);
     }
 }));
 
+//DELETE existing AssetID
+router.delete('account/:account-id/asset/:asset-id', asyncHandler(async( req, res, next) => {
+    try{
+      const asset = await Asset.findByPk(req.params.id);
+    //   if (asset.id !== req.currentUser.id){
+    //     const error = new Error("You are not authorized to delete this course");
+    //     error.status = 401;
+    //     next(error);
+    //   }
+      if (asset.id === req.currentUser.id){
+        await asset.destroy();
+        res.status(204).end()
+      } else{
+        const error = new Error("The liability you're trying to find doesn't exist");
+        error.status = 404;
+        next(error);
+      }
+    }
+  }));
+//DELETE existing LiabilityID
+router.delete('account/:id/liability/:liability-id', asyncHandler(async( req, res, next) => {
+    try{
+      const liability = await Liability.findByPk(req.params.id);
+    //   if (asset.id !== req.currentUser.id){
+    //     const error = new Error("You are not authorized to delete this course");
+    //     error.status = 401;
+    //     next(error);
+    //   }
+      if (liability.id === req.currentUser.id){
+        await liability.destroy();
+        res.status(204).end()
+      } else{
+        const error = new Error("The liability you're trying to find doesn't exist");
+        error.status = 404;
+        next(error);
+      }
+    }
+  }));
 
-//DELETE AssetID
+//UPDATE Existing AssetID
+router.put('account/:account-id/asset/:asset-id', asyncHandler(async( req, res) => {
+    try{
+        const asset = await Asset.findByPk(req.params.id);
+    //   if (course.userId !== req.currentUser.id){
+    //     const error = new Error("You are not authorized to update this course");
+    //     error.status = 401;
+    //     next(error);
+    //   }
+      if (asset){
+        await asset.update(req.body)
+        res.status(204).end();
+      } else{
+        const error = new Error("The asset you're trying to find doesn't exist");
+        error.status = 404;
+        next(error);
+      }
+    } 
+  }));
+//UPDATE Existing LiabilityID
+router.put('account/:id/liability/:liability-id', asyncHandler(async( req, res) => {
+    try{
+        const liability = await Liability.findByPk(req.params.id);
+    //   if (course.userId !== req.currentUser.id){
+    //     const error = new Error("You are not authorized to update this course");
+    //     error.status = 401;
+    //     next(error);
+    //   }
+      if (asset){
+        await asset.update(req.body)
+        res.status(204).end();
+      } else{
+        const error = new Error("The liability you're trying to find doesn't exist");
+        error.status = 404;
+        next(error);
+      }
+    } 
+  }));
 
-//DELETE DebtID
+//CREATE New AccountID
+router.post('account/:account-id', asyncHandler(async (req, res) => {
+    const errors = [];
+    try{
+      const account = await Account.create(req.body);
+    if (errors.length > 0) {
+      // Return the validation errors to the client.
+      res.status(400).json({ errors });
+    } 
+    // Set the status to 201 Created and end the response.
+    res.status(201).location("/").end();
+    
+    } catch(error){
+      if(error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') { 
+        const errors = error.errors.map(err => err.message);
+        res.status(400).json({ errors });
+      } else {
+        throw error;
+      } 
+    }
+  }));
 
-//UPDATE AssetID
-//UPDATE DebtID
+//CREATE New AssetID
+router.post('account/:account-id/asset/asset-id', asyncHandler(async (req, res) => {
+    const errors = [];
+    try{
+      const asset = await Asset.create(req.body);
+    if (errors.length > 0) {
+      // Return the validation errors to the client.
+      res.status(400).json({ errors });
+    } 
+    // Set the status to 201 Created and end the response.
+    res.status(201).location("/").end();
+    
+    } catch(error){
+      if(error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') { 
+        const errors = error.errors.map(err => err.message);
+        res.status(400).json({ errors });
+      } else {
+        throw error;
+      } 
+    }
+  }));
 
-//CREATE User AccountID
-//CREATE AssetID
-//CREATE DebtID
+//CREATE New LiabilityID
+router.post('account/:account-id/liability/:liability-id', asyncHandler(async (req, res) => {
+    const errors = [];
+    try{
+      const liability = await Liability.create(req.body);
+    if (errors.length > 0) {
+      // Return the validation errors to the client.
+      res.status(400).json({ errors });
+    } 
+    // Set the status to 201 Created and end the response.
+    res.status(201).location("/").end();
+    
+    } catch(error){
+      if(error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') { 
+        const errors = error.errors.map(err => err.message);
+        res.status(400).json({ errors });
+      } else {
+        throw error;
+      } 
+    }
+  }));
